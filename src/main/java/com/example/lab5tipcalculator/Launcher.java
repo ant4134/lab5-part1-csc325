0b6f724ef436422ec7f671181d9eaec0d724d051
anthony S <ant4130499@gmail.com>
2025-10-05 15:02:16 -0400

    first commit

---
+++ src/main/java/com/example/lab5tipcalculator/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.lab5tipcalculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(TipCalculator.class, args);
+    }
+}
